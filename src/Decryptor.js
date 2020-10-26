0c625ec3232b6c1b55a19fd07e39cd16af907661
Tischa <tischa.nyanguile@student.ehb.be>
2020-10-26 19:42:02 +0100

    Edit Decryptor.js

--- src/Decryptor.js
+++ src/Decryptor.js
@@ -2,8 +2,9 @@ const nacl = require('libsodium-wrappers')
 
 module.exports = async (key) => {
     await nacl.ready;
+    if(key === undefined) throw 'no key';
     
     return Object.freeze({
-        decrypt : (ciphertext, nonce) => crypto_secretbox_open(ciphertext, nonce)
+        decrypt : (ciphertext, nonce) => nacl.crypto_secretbox_open_easy(ciphertext, nonce, key)
     });
 }
--- src/PasswordHashVerifier.js
+++ src/PasswordHashVerifier.js
@@ -2,7 +2,7 @@ const nacl = require('libsodium-wrappers')
 
 module.exports = async () => {
     await nacl.ready;
-
+    
     return Object.freeze({
         verify : (hashedPw, pw) => nacl.crypto_pwhash_str_verify(hashedPw, pw)
     });
