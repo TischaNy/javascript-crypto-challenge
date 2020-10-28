d11d1a46a976cef23db5461700d3e291fb30a08a
Tischa <tischa.nyanguile@student.ehb.be>
2020-10-28 17:55:40 +0100

    Code clean up in SecureSessionPeer

--- src/SecureSessionPeer.js
+++ src/SecureSessionPeer.js
@@ -4,31 +4,23 @@ const Encryptor = require('./Encryptor.js');
 
 let encryptor;
 let decryptor;
-let server = {}
-let client = {}
+
 let result = {}
 
 module.exports = async (otherPeer) => {
     await nacl.ready;
- 
-    let serverSessionKeys;
-    let clientSessionKeys;
+    
     let keys = nacl.crypto_box_keypair();
 
-    if(otherPeer){
-        server = nacl.crypto_kx_keypair();
-        client = nacl.crypto_kx_keypair();
-        clientSessionKeys = nacl.crypto_kx_client_session_keys(client.publicKey, client.privateKey, server.publicKey);
+    if(otherPeer){  
+        let server = nacl.crypto_kx_keypair();
+        let client = otherPeer.setServer(server.publicKey);
+        clientSessionKeys = client.sharedKeys;
         serverSessionKeys = nacl.crypto_kx_server_session_keys(server.publicKey, server.privateKey, client.publicKey);
+  
         encryptor = await Encryptor(clientSessionKeys.sharedTx);
         decryptor = await Decryptor(serverSessionKeys.sharedRx);
     }
-
-    if(encryptor === undefined && decryptor == undefined){
-        encryptor = await Encryptor(keys.publicKey);
-        decryptor = await Decryptor(keys.privateKey);
-    }   
-
     return Object.freeze({
         publicKey : keys.publicKey,
         encrypt : (msg) => {
@@ -50,6 +42,14 @@ module.exports = async (otherPeer) => {
                 result = {};
             }
             return msg;
+        },
+
+        setServer : (server) => {
+            let client = nacl.crypto_kx_keypair();
+            return {
+                publicKey : client.publicKey,
+                sharedKeys : nacl.crypto_kx_client_session_keys(client.publicKey, client.privateKey, server)
+            }
         }
     });
 }
