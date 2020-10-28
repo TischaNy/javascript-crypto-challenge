82a5cc70e3338f6329aad687a9d8ce049c710e6e
Tischa <tischa.nyanguile@student.ehb.be>
2020-10-28 15:39:22 +0100

    Finish SecureSessionPeer

--- src/SecureSessionPeer.js
+++ src/SecureSessionPeer.js
@@ -1,50 +1,33 @@
 const nacl = require('libsodium-wrappers')
 
 let keys;
-let nonce;
-let result;
-let tx;
-let rx;
-
-const server = {
-    publicKey : undefined,
-    privateKey : undefined
-}
-
-const client = {
-    publicKey : undefined,
-    privateKey : undefined
-}
- 
+let server = {}
+let client = {}
+let result = {}
 
 module.exports = async (otherPeer) => {
     await nacl.ready;
     keys = nacl.crypto_box_keypair();
 
     if(otherPeer){
-        let server_keys = nacl.crypto_kx_keypair();
-        server.publicKey = server_keys.publicKey
-        server.privateKey = server_keys.privateKey;
+        server = nacl.crypto_kx_keypair();
     }else{
-        let client_keys = nacl.crypto_kx_keypair();
-        client.publicKey = client_keys.publicKey
-        client.privateKey = client_keys.privateKey;
+        client = nacl.crypto_kx_keypair();
     }
 
     return Object.freeze({
         publicKey : keys.publicKey,
         encrypt : (msg) => {
-            nonce = nacl.randombytes_buf(nacl.crypto_secretbox_NONCEBYTES);
-            let sharedKeys = nacl.crypto_kx_client_session_keys(keys.publicKey, keys.privateKey, server.publicKey);
-       
+            let sharedKeys = nacl.crypto_kx_client_session_keys(client.publicKey, client.privateKey, server.publicKey);
+            let nonce = nacl.randombytes_buf(nacl.crypto_secretbox_NONCEBYTES);
             return {
                 ciphertext: nacl.crypto_secretbox_easy(msg, nonce, sharedKeys.sharedTx),
                 nonce : nonce
             }
-     
         },
 
         decrypt : (ciphertext, nonce) => {
+            let sharedKeys = nacl.crypto_kx_server_session_keys(server.publicKey, server.privateKey, client.publicKey);
             return nacl.crypto_secretbox_open_easy(ciphertext, nonce, sharedKeys.sharedRx);
         },
 
@@ -57,17 +40,13 @@ module.exports = async (otherPeer) => {
             }
         },
 
-        
-
         receive : () => {
             if(result){
                 let sharedKeys = nacl.crypto_kx_server_session_keys(server.publicKey, server.privateKey, client.publicKey);
                 msg = nacl.crypto_secretbox_open_easy(result.ciphertext, result.nonce, sharedKeys.sharedRx);
             }
-            
+            result = {};
             return msg;
         }
-
-  
     });
 }
